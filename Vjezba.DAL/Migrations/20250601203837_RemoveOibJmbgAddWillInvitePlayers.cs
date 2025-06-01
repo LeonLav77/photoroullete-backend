512c795cb059ba56ce6885659e772d08685c0404
LeonLav77 <leonlav77@gmail.com>
2025-06-02 00:13:02 +0200

    dev change

--- Vjezba.DAL/Migrations/20250601203837_RemoveOibJmbgAddWillInvitePlayers.cs
+++ Vjezba.DAL/Migrations/20250601203837_RemoveOibJmbgAddWillInvitePlayers.cs
@@ -10,13 +10,42 @@ namespace Vjezba.DAL.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropColumn(
+                name: "OIB",
+                table: "AspNetUsers");
 
+            migrationBuilder.DropColumn(
+                name: "JMBG",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "WillInvitePlayers",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropColumn(
+                name: "WillInvitePlayers",
+                table: "AspNetUsers");
 
+            migrationBuilder.AddColumn<string>(
+                name: "OIB",
+                table: "AspNetUsers",
+                type: "TEXT",
+                maxLength: 11,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "JMBG",
+                table: "AspNetUsers",
+                type: "TEXT",
+                maxLength: 13,
+                nullable: true);
         }
     }
 }
